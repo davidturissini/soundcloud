1c341b0ffcb1960aa57d666909d9983215159885
Dave Turissini <davidairborne@gmail.com>
2014-02-19 22:24:13 -0800

    Adding joinPaginated method

--- src/soundcloud.js
+++ src/soundcloud.js
@@ -11,6 +11,42 @@ var soundcloud = {
 	configure: function (config) {
 		defaults = _.extend(defaults, config);
 	},
+
+	joinPaginated: function (url, limit, max) {
+		var data = [];
+		var promises = [];
+		var offset = 0;
+		var offsets = [];
+		max = (max > 8000) ? 8000 : max;
+
+		while(offset <= max && max !== 0) {
+			(function (o) {
+
+				var promise = soundcloud.api(url, {
+					limit:limit,
+					offset:o
+				})
+
+				.then(function (returnedData) {
+					data = data.concat(returnedData);
+				})
+
+				promises.push(promise);
+			}(offset));
+
+			if (offset < max && limit + offset > max && max === 8000) {
+				offset = max;
+			} else {
+				offset += limit;
+			}
+
+		}
+
+		return q.all(promises)
+			.then(function () {
+				return data;
+			})
+	},
 	
 	api: function (path, requestOptions) {
 
@@ -19,7 +55,7 @@ var soundcloud = {
 		apiHost = 'api.soundcloud.com',
 		url;
 		
-		url = 'http://' + apiHost + path + '.json';//?' + querystring.stringify(options);
+		url = 'http://' + apiHost + path + '.json';
 
 		return pigeon.get(url, _.extend(defaults, requestOptions || {})).then(JSON.parse);
 		
